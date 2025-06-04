7d4cdcc27d6cdae99f61a71f3756d07a7b86ee49
Johan Manuel Vicente B <jomavi582@gmail.com>
2025-06-04 10:32:40 -0400

    Create the class abstract Person a CommunityMember

---
+++ HomeworkITLA/Homework1/Homework1/Classes/AbstractsClasses/CommunityMember.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework1.Classes.AbstractsClasses
+{
+    public abstract class CommunityMember : Person
+    {
+        public CommunityMember(int Cedula, string Name, Apellido) { }
+    }
+}
---
+++ HomeworkITLA/Homework1/Homework1/Classes/AbstractsClasses/Person.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework1.Classes.AbstractsClasses
+{
+    public abstract class Person
+    {
+        public  required int Cedula { get; set; }
+        public required string Name { get; set; }
+        public required string Apellido { get; set; }
+        public required int Age { get; set; }
+
+        public string? Address { get; set; }
+
+        public Person(int Cedula, string Name, string Apellido, int Age, string? Address)
+        {
+            this.Cedula = Cedula;
+            this.Name = Name;
+            this.Apellido = Apellido;
+            this.Age = Age;
+            this.Address = Address;
+
+        }
+
+        public abstract void ShowInfo();
+
+    }
+}
