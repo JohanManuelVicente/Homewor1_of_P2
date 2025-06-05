38d620086fe199d4236b3b5d74d603aeee3c0095
Johan Manuel Vicente B <jomavi582@gmail.com>
2025-06-04 21:26:42 -0400

    Update

--- HomeworkITLA/Homework1/Homework1/Program.cs
+++ HomeworkITLA/Homework1/Homework1/Program.cs
@@ -5,7 +5,12 @@ using Homework1.Classes.SubClasses;
 
 
 
-Student Student1 = new Student(4022548844, "Johan", "Lopez", 22, "San Jose", "Estudiante", "Ingenieria", "2024-1260");
+var Student1 = new Student(4022548844, 
+    "Johan", "Lopez", 22, "San Jose", "Estudiante", "Ingenieria", "2024-1260"
+    
+    
+    
+    );
 
 
 
