fec544a0aeeae0fe534f449a9bb0475de24ec080
Johan Manuel Vicente B <jomavi582@gmail.com>
2025-06-04 23:12:39 -0400

    Finishinf Program

--- HomeworkITLA/Homework1/Homework1/Program.cs
+++ HomeworkITLA/Homework1/Homework1/Program.cs
@@ -7,7 +7,6 @@ var Student1 = new Student(4022548844,
     "Johan", "Lopez", 22, "San Jose", "Estudiante", "Ingenieria", "2024-1260"
     );
 
-
 var exStudent2 = new ExStudent(
     10101010,
     "María",
@@ -19,7 +18,6 @@ var exStudent2 = new ExStudent(
     "B20251337",
     new DateTime(2023, 2, 15)
 );
-
 
 var Employee1 = new Employees(
 
@@ -33,7 +31,6 @@ var Employee1 = new Employees(
     "Docente",
     "Desarrollo de Software"
     );
-
 
 var Teaching1 = new Teaching(
     4022165454,
@@ -50,16 +47,15 @@ var Teaching1 = new Teaching(
     "Programacion 1"
     );
 
-
 var Teacher1 = new Teacher(
     4022165454,
-    "Carlos",
-    "Tavares",
+    "Jose",
+    "Linares",
     30,
     "Jacobo",
     "Empleado",
-    30000,
-    "Docente",
+    40000,
+    "Maestro",
     "Desarrollo de Software",
     "Master",
     14,
@@ -78,34 +74,57 @@ var Administrative1 = new Administrative(
     "De Jesus",
     30,
     "Jacobo",
-    "Administrativo",
+    "Empleado",
     60000,
     "Docente",
     "Desarrollo de Software"
 
     );
 
-//Student1.ShowInfo();
+var Adnmin1 = new Administrators(
+     4022165454,
+    "Ruth",
+    "Gonzalez",
+    45,
+    "D.N.",
+    "Empleado",
+    70000,
+    "Administrador",
+    "Desarrollo de Software",
+    "PhD",
+    34,
+    "Programacion paralela",
+    "Gestionar docentes",
+    25
 
-//Console.WriteLine();
+    );
 
-//exStudent2.ShowInfo();
 
-//Console.WriteLine();
 
-//Employee1.ShowInfo();
+Student1.ShowInfo();
 
-//Console.WriteLine();
+Console.WriteLine();
 
-//Teaching1.ShowInfo();
+exStudent2.ShowInfo();
 
-//Console.WriteLine();
+Console.WriteLine();
 
-//Administrative1.ShowInfo();
+Employee1.ShowInfo();
 
-//Console.WriteLine();
+Console.WriteLine();
 
-//Teacher1.ShowInfo();
+Teaching1.ShowInfo();
 
+Console.WriteLine();
+
+Administrative1.ShowInfo();
+
+Console.WriteLine();
+
+Teacher1.ShowInfo();
+
+Console.WriteLine();
+
+Adnmin1.ShowInfo();
 
 
