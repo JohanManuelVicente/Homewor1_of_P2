79e3d7cd8f142456ee18233b8502e8607f4c24b8
Johan Manuel Vicente B <jomavi582@gmail.com>
2025-06-04 21:45:22 -0400

    Create Employees

---
+++ HomeworkITLA/Homework1/Homework1/Classes/SubClasses/Employees.cs
@@ -0,0 +1,23 @@
+﻿using Homework1.Classes.AbstractsClasses;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework1.Classes.SubClasses
+{
+    public class Employees : CommunityMember
+    {
+        public string Department { get; set; }
+        public Employees(long Cedula, string Name, string Apellido, int Age, string? Address, string Rol) : base(Cedula, Name, Apellido, Age, Address, Rol)
+        {
+
+        }
+
+        public override void ShowInfo()
+        {
+            
+        }
+    }
+}
